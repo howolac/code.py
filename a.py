34b14941c781799f07b8c86ee1f718dc594f3198
howolac <858103451@qq.com>
2021-03-02 23:13:56 +0800

    征服一条龙的奖励加到原有的数据中去打印出来

--- a.py
+++ a.py
@@ -1,12 +1,30 @@
-item = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-
-
-def displayInventory(ie):
+def displayInventory(item):
     count = 0
-    for k, v in ie.items():
-        print(str(v) + ' ' + k)
+    for k, v in item.items():
+        print(k + ' ' + str(v))
         count = count + v
-    return count
 
 
-print(displayInventory(item))
+def addedItems(inv, item):
+    sum = 0
+    for j in inv.values():
+        sum += j
+    for i in item:
+        if i in inv.keys():
+            inv[i] += 1
+            sum += 1
+
+        else:
+            inv.setdefault(i, 1)
+            sum += 1
+
+    print(sum)
+    return inv
+
+
+item = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = {'gold coin': 42, 'rope': 1}
+
+inv = addedItems(inv, item)
+
+displayInventory(inv)
