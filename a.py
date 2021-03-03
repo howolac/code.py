498af9973cc543ed0470221cf87b4ad07fe6050f
howolac <858103451@qq.com>
2021-03-04 00:14:11 +0800

    查找电话号码的正则表达式实现

--- a.py
+++ a.py
@@ -1,29 +1,9 @@
 #！python3
-#phone number found
+#正则表达式识别
 
+import re
 
-def isPhonenum(text):
-    if len(text) != 12:
-        return False
-
-    for i in range(0, 3):
-        if not text[i].isdecimal():
-            return False
-    if text[3] != '-':
-        return False
-
-    for i in range(4, 7):
-        if not text[i].isdecimal():
-            return False
-    if text[7] != '-':
-        return False
-    else:
-        return True
-
-
-message = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.415-555-9999'
-
-for i in range(len(message) - 11):
-    if isPhonenum(message[i:i + 12]):
-        print('phonenumber found , it\'s ' + str(message[i:i + 12]))
-print('done')
+phonenum = re.compile(r'\d{3}-\d{3}-\d{3}')
+a = 'My number is 415-555-4242.'
+mo = phonenum.search(a)
+print('phone number found ' + mo.group())
