d7600daafff6fcea6dfad8a2dad49ed55fe0fe13
howolac <858103451@qq.com>
2021-03-03 16:35:06 +0800

    一个小小的口令脚本

--- a.py
+++ a.py
@@ -1,10 +1,27 @@
 #！python3
 ''' 
-一个不断变化的程序
+一个口令小脚本
 
 '''
 
-spam = 'SpamSpamBaconSpamEggsSpamSpam'
+import sys, pyperclip
 
-print(spam.strip('Spam'))
+passwd = {
+    'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+    'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+    'luggage': '12345'
+}
 
+if len(sys.argv) < 2:
+    print('Usage: python a.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]
+
+if account in passwd:
+    print('the passwd is' + ' ' + passwd[account])
+    pyperclip.copy(passwd[account])
+    print('the account' 'is' + ' ' + account)
+
+else:
+    print('not match')
