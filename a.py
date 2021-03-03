655c893a13d87a6e4d1452de75d1954fbd541022
howolac <858103451@qq.com>
2021-03-03 09:10:04 +0800

    请输入账号和密码之字符串处理

--- a.py
+++ a.py
@@ -1,30 +1,13 @@
-def displayInventory(item):
-    count = 0
-    for k, v in item.items():
-        print(k + ' ' + str(v))
-        count = count + v
+while True:
+    print('enter your age:')
+    age = input()
+    if age.isdecimal():
+        break
+    print('please enter your ager:')
 
-
-def addedItems(inv, item):
-    sum = 0
-    for j in inv.values():
-        sum += j
-    for i in item:
-        if i in inv.keys():
-            inv[i] += 1
-            sum += 1
-
-        else:
-            inv.setdefault(i, 1)
-            sum += 1
-
-    print(sum)
-    return inv
-
-
-item = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-inv = {'gold coin': 42, 'rope': 1}
-
-inv = addedItems(inv, item)
-
-displayInventory(inv)
+while True:
+    print('Please select a new passwd (alpha and number only):')
+    passwd=input()
+    if passwd.isalnum():
+            break
+    print('passwd can only include letters and numbers')
