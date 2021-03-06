cd0f2a6bd8ef3bb85b58c63da9ff4f2aae9122e9
howolac <858103451@qq.com>
2021-03-06 14:04:37 +0800

    匹配短句

--- a.py
+++ a.py
@@ -1,15 +1,20 @@
 #！python3
 #正则表达式
-
+#匹配短句
 import re, pyperclip
 
-regex = re.compile(r'((\d{1,3})(,\d{3})*)')
-print(regex.findall('''
-'42'
-'1,234'
-'6,368,745'
+regex = re.compile(
+    r'((alice|Bob|carol)\s(eats|pets|throws)\s(apple|cats|baseball))',
+    re.IGNORECASE)
+print(
+    regex.findall('''
+ 'Alice eats apples.'
+ 'Bob pets cats.'
+ 'Carol throws baseballs.'
+ 'Alice throws Apples.'
+ 'BOB EATS CATS.'
+但不匹配：
+ 'RoboCop eats apples.'
+ 'ALICE THROWS FOOTBALLS.'
+ 'Carol eats 7 cats.'
 '''))
-
-'42'
-'1,234'
-'6,368,745'
