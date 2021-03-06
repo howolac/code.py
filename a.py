db596e61089f14e0e913c478b6150339b908d696
howolac <858103451@qq.com>
2021-03-06 13:51:03 +0800

    匹配数字

--- a.py
+++ a.py
@@ -1,20 +1,15 @@
 #！python3
 #正则表达式
-#匹配名字
 
 import re, pyperclip
 
-regex = re.compile(r'([A-Z][a-z]+\s[A-Z][a-z]+)')
-print(
-    regex.findall('''该正则表达式必须匹配：
- 'Satoshi Nakamoto'
- 'Alice Nakamoto'
- 'RoboCop Nakamoto'
-但不匹配：
- 'satoshi Nakamoto'（名字没有大写首字母）
- 'Mr. Nakamoto'（前面的单词包含非字母字符）
- 'Nakamoto' （没有名字）
- 'Satoshi nakamoto'（姓没有首字母大写）
-
-'Mr. Nakamoto'
+regex = re.compile(r'((\d{1,3})(,\d{3})*)')
+print(regex.findall('''
+'42'
+'1,234'
+'6,368,745'
 '''))
+
+'42'
+'1,234'
+'6,368,745'
