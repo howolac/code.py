ea91cd1b71c15a4d6f330c109026690a484b0515
howolac <858103451@qq.com>
2021-03-06 13:49:48 +0800

    匹配名字

--- a.py
+++ a.py
@@ -1,44 +1,20 @@
 #！python3
 #正则表达式
+#匹配名字
 
 import re, pyperclip
 
-emailregext = re.compile(
-    r'''(
-[a-zA-Z0-9._%+]+ #username
-@   #@ symbol
-[0-9a-zA-Z.-]+ #domain name
-(\.[a-zA-Z]{2,4}) #dot-something
+regex = re.compile(r'([A-Z][a-z]+\s[A-Z][a-z]+)')
+print(
+    regex.findall('''该正则表达式必须匹配：
+ 'Satoshi Nakamoto'
+ 'Alice Nakamoto'
+ 'RoboCop Nakamoto'
+但不匹配：
+ 'satoshi Nakamoto'（名字没有大写首字母）
+ 'Mr. Nakamoto'（前面的单词包含非字母字符）
+ 'Nakamoto' （没有名字）
+ 'Satoshi nakamoto'（姓没有首字母大写）
 
-)''', re.VERBOSE)
-
-emailmatch = []
-text = str(pyperclip.paste())
-
-for i in emailregext.findall(text):
-    emailmatch.append(i[0])
-
-phoneamtch = []
-phontregext = re.compile(
-    r'''(
-(\d{3}|\(\d{3}\))? #area code
-(\s|-|\.)?                 #separator|
-(\d{3})                  #first 3 digits
-(\.|-|\s)    # separator
-(\d{4})     #last 4 digits
-(\s*(ext|x|ext.)\s*\d{3,5})?        #extension
-
-)''', re.VERBOSE)
-
-for i in phontregext.findall(text):
-    phoneamtch.append(i[0])
-
-#这里简化只写一个
-if len(emailmatch) > 0:
-    pyperclip.copy('\n'.join(emailmatch) + ''.join(phoneamtch))
-    print('emails:')
-    print('\n'.join(emailmatch))
-    print('phonenums:')
-    print('\n'.join(phoneamtch))
-else:
-    print('no email match at least')
+'Mr. Nakamoto'
+'''))
