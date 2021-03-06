ceb6ddf50af88a6e00298c952d59c30169ddc935
howolac <858103451@qq.com>
2021-03-06 16:20:58 +0800

    做类似strip()的事

--- a.py
+++ a.py
@@ -1,15 +1,30 @@
 #！python3
 #正则表达式
-#强口令检测
+'''如果只传入了要去除的字符串，没有其他参数，那么就从该字符串首尾去除空白字符。否
+则，函数第二个参数指定的字符将从该字符串中去除。
+'''
 
-import re
+import re, pyperclip
 
-a = '1236aZa789'
 
-try:
-    re.compile(r'[a-z]').search(a).group()
-    re.compile(r'[A-Z]').search(a).group()
-    re.compile(r'[0-9]').search(a).group()
-    print('强字符串')
-except:
-    print('not')
+def sta(x, y=''):
+    if y == '':
+        regex = re.compile(r'(\s*)(((.)+)(\S))(\s*)', re.DOTALL)
+        t = regex.search(x)
+
+        print('clear it!')
+
+        return t.group(2)
+    else:
+        t = []
+
+        for i in range(len(x)):
+            if x[i] != y:
+                t += x[i]
+
+        print('clear ', y)
+
+        return ''.join(t)
+
+
+pyperclip.copy(sta(pyperclip.paste()))
