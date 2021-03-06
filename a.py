521c0467d9d97e9eedbfbfeeaf41db2251e17d75
howolac <858103451@qq.com>
2021-03-06 19:29:57 +0800

    文件的覆写和添加

--- a.py
+++ a.py
@@ -1,30 +1,14 @@
 #！python3
-#正则表达式
-'''如果只传入了要去除的字符串，没有其他参数，那么就从该字符串首尾去除空白字符。否
-则，函数第二个参数指定的字符将从该字符串中去除。
-'''
+#读写本地文件
 
-import re, pyperclip
+import os
 
+os.chdir('c:\\users\\name\\desktop')
 
-def sta(x, y=''):
-    if y == '':
-        regex = re.compile(r'(\s*)(((.)+)(\S))(\s*)', re.DOTALL)
-        t = regex.search(x)
+baconFile = open('bacon.txt', 'w')
+print(baconFile.write('Hellso,File\n'))
+baconFile.close()
 
-        print('clear it!')
-
-        return t.group(2)
-    else:
-        t = []
-
-        for i in range(len(x)):
-            if x[i] != y:
-                t += x[i]
-
-        print('clear ', y)
-
-        return ''.join(t)
-
-
-pyperclip.copy(sta(pyperclip.paste()))
+baconFile = open('bacon.txt', 'a')
+print(baconFile.write('Helslo,Fdile\n'))
+baconFile.close()
