503a3cac939184ff79463f6c7c3e46c8ff1cb03b
howolac <858103451@qq.com>
2021-03-07 21:25:42 +0800

    增加打开文件参数。。打开文件夹中所有的.txt 文件

--- a.py
+++ a.py
@@ -1,32 +1,32 @@
 #！python3
 #项目：打开文件夹中所有的.txt 文件，查找匹配用户提供的正则表达式的所有行。
 #
-#用法：txt文件名字中间有一个空格不行,path add your path
+#用法：txt文件名字中间有一个空格的不行→可以！！！
 
 import os, re
 
-path = 'C:\\?'
+#改变当前路径
+path = 'path'
+os.chdir(path)
 
-
-txt_file = os.listdir('.')
-for i in range(len(txt_file)):
-    if txt_file[i][-1] == 't' and txt_file[i][-2] == 'x' and txt_file[i][
-            -3] == 't' and txt_file[i][-4] == '.':
-        continue
-    else:
-        del txt_file[i]
-
-
+#遍历文件列表
+a = os.listdir()
+#创建一个找txt文件的正则表达式
 regex = re.compile(r'字符串')
 
-for i in range(len(txt_file)):
-    file = open(os.path.join(path, txt_file[i]), encoding='utf8')
-    a = file.readlines()
+for i in range(len(a)):
+    #判断是否log文件
+    if a[i][-1] == 't' and a[i][-2] == 'x' and a[i][-3] == 't':
+        #打开文件
+        file = open(a[i], 'r', encoding='utf8')
+        #便利每个文件的每一行
+        b = file.readlines()
 
-    for j in range(len(a)):
-        try:
-            regex.search(a[j]).group()
-            print(a[j])
-        except:
-            continue
-
+        for j in range(len(b)):
+            #找出这所有有字符串的行，并打印出来
+            try:
+                regex.search(b[j]).group()
+                print(b[j])
+            except:
+                continue
+        file.close()
