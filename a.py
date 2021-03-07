9603e59abc61743ed665ae7ea8537675a69c3077
howolac <858103451@qq.com>
2021-03-07 18:48:32 +0800

    找出一个文件夹下所有txt文件中的带有字符串(可自定)的所有行s

--- a.py
+++ a.py
@@ -1,17 +1,32 @@
 #！python3
-#项目：疯狂填词
-#用法：根据程序提示，对一个语句中的一些词进行替换
+#项目：打开文件夹中所有的.txt 文件，查找匹配用户提供的正则表达式的所有行。
+#
+#用法：txt文件名字中间有一个空格不行,path add your path
 
-a = '''The ADJECTIVE panda walked to the NOUN and then VERB. A nearby NOUN was
-unaffected by these events.'''
+import os, re
 
-#分割字符串
-a = a.split(' ')
+path = 'C:\\?'
 
-#找到那些地方，提示用户输入以替换
-for i in range(len(a)):
-    if a[i] == 'ADJECTIVE' or a[i] == 'NOUN' or a[i] == 'ADVERB' or a[
-            i] == 'VERB':
-        print('Enter an ' + a[i].lower())
-        a[i] = input()
-print(' '.join(a))
+
+txt_file = os.listdir('.')
+for i in range(len(txt_file)):
+    if txt_file[i][-1] == 't' and txt_file[i][-2] == 'x' and txt_file[i][
+            -3] == 't' and txt_file[i][-4] == '.':
+        continue
+    else:
+        del txt_file[i]
+
+
+regex = re.compile(r'字符串')
+
+for i in range(len(txt_file)):
+    file = open(os.path.join(path, txt_file[i]), encoding='utf8')
+    a = file.readlines()
+
+    for j in range(len(a)):
+        try:
+            regex.search(a[j]).group()
+            print(a[j])
+        except:
+            continue
+
