f4bc7a52e3a4b8f5da5a57db2df3fdd491ae7c8e
howolac <858103451@qq.com>
2021-03-08 22:34:21 +0800

    把一个文件下下的所有某种扩展的文件复制到一个目标目录

--- a.py
+++ a.py
@@ -1,49 +1,19 @@
-#！python3
-#项目：批量将美国风格的日期替换成欧洲风格的日期
-#usage：把有美国风格日期的有txt的文件夹的路径加入进去，替换成欧洲风格的日期
-#不能移动给itself。。。→要指明文件！
-#regext.search()返回None或着可以用try： except：
+#!python
 
-import os, re, shutil
+#项目：遍历一个目录树，查找特定扩展的文件（.txt），将它们拷贝到一个新的文件夹
 
-os.chdir('c:\\users\\howolac\\desktop')
+#usage：向path传入目录就行了
 
-path = 'c:\\users\howolac\\desktop'
-#用os.listdir()找到所有文件名
-filenames = os.listdir(path)
-#创建一个识别美国日期的正则表达式
-regex = re.compile(
-    r'''(^(.*?)
-    ((0|1)?\d)-  #一个或两个月的数字
-    ((0|1|2|3)?\d)-(  #一个或两个关于日的数字
-    ((19|20)\d\d)   #四个关于年份的数字
-    (.*)?       #非贪心匹配剩下的所有字符  
-#这里可以试着替换成(.*?)   ，结果会是这个不匹配字符 (.*)?  like (.*),just have or not
+import os, shutil
 
-))''', re.VERBOSE)
-#保存下面筛选出来的文件d额list
-kongnew = []
-kongold = []
-#遍历每一个文件来筛选出带有美国风格的日期
-#bing修改美国风格的日期成欧洲风格的
-for i in filenames:
-    try:
-        regex.search(i).group()
-        print
-        kongold += [i]
-        #用括号调换一下中间位置和第一个位置
-        #最好一个变量一个变量列出来而不是一次性写出来
-        kongnew += [
-            regex.search(i).group(2) + regex.search(i).group(5) + '-' +
-            regex.search(i).group(3) + '-' + regex.search(i).group(7)
-        ]
-    except:
-        continue
+path_source = 'D:\\Program Files'
+path_direction = 'C:\\users\\howolac\\desktop'
 
-#用shutil.move()函数来移动文件
-#for 从当前路径移动到当前路径
-for i in range(len(kongnew)):
-    print('Changing ' + kongold[i] + 'to ' + kongnew[i])
-    shutil.move(kongold[i], kongnew[i])
+#判断是不是
 
-print('done')
+for folder, sub_folders, file_names in os.walk(path_source):
+    #判断文件是不是.dat结尾的,并把他们拷贝到桌面
+    for i in file_names:
+        if i.endswith('.dat'):
+            shutil.copy(os.path.join(os.path.abspath(folder), i), path_direction)
+            print('find ' + i)
