59a4b3ca1c205c9a37906039781b48512df6d37c
howolac <858103451@qq.com>
2021-03-08 15:26:30 +0800

    批量将美国风格的日期替换成欧洲风格的日期

--- a.py
+++ a.py
@@ -1,14 +1,49 @@
 #！python3
-#遍历目录树
-#usage：压缩文件。注意先zip.namelist()之后才知道文件的名字，可能是a/a.txt
-#但不是a.txt
-#压缩文件夹似乎不会压缩文件夹里面的文件
+#项目：批量将美国风格的日期替换成欧洲风格的日期
+#usage：把有美国风格日期的有txt的文件夹的路径加入进去，替换成欧洲风格的日期
+#不能移动给itself。。。→要指明文件！
+#regext.search()返回None或着可以用try： except：
 
-import os, zipfile
+import os, re, shutil
 
 os.chdir('c:\\users\\howolac\\desktop')
 
-zip = zipfile.ZipFile('a.zip', 'w')
-zip.write('sasa.txt', compress_type=zipfile.ZIP_DEFLATED)
+path = 'c:\\users\howolac\\desktop'
+#用os.listdir()找到所有文件名
+filenames = os.listdir(path)
+#创建一个识别美国日期的正则表达式
+regex = re.compile(
+    r'''(^(.*?)
+    ((0|1)?\d)-  #一个或两个月的数字
+    ((0|1|2|3)?\d)-(  #一个或两个关于日的数字
+    ((19|20)\d\d)   #四个关于年份的数字
+    (.*)?       #非贪心匹配剩下的所有字符  
+#这里可以试着替换成(.*?)   ，结果会是这个不匹配字符 (.*)?  like (.*),just have or not
 
-zip.close()
+))''', re.VERBOSE)
+#保存下面筛选出来的文件d额list
+kongnew = []
+kongold = []
+#遍历每一个文件来筛选出带有美国风格的日期
+#bing修改美国风格的日期成欧洲风格的
+for i in filenames:
+    try:
+        regex.search(i).group()
+        print
+        kongold += [i]
+        #用括号调换一下中间位置和第一个位置
+        #最好一个变量一个变量列出来而不是一次性写出来
+        kongnew += [
+            regex.search(i).group(2) + regex.search(i).group(5) + '-' +
+            regex.search(i).group(3) + '-' + regex.search(i).group(7)
+        ]
+    except:
+        continue
+
+#用shutil.move()函数来移动文件
+#for 从当前路径移动到当前路径
+for i in range(len(kongnew)):
+    print('Changing ' + kongold[i] + 'to ' + kongnew[i])
+    shutil.move(kongold[i], kongnew[i])
+
+print('done')
