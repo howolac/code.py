a7a01d1f7dbe308f6d2a0a4263be41cab23f0094
howolac <858103451@qq.com>
2021-03-08 23:34:31 +0800

    计算所有大于10gb的文件夹

--- a.py
+++ a.py
@@ -1,7 +1,6 @@
 #!python
 
-#项目：计算某文件夹下所有大于1gb文件
-
+#项目：计算所有大于10gb的文件夹
 #usage：向path传入目录就行了
 #传入'c:\\users\\用户'会出现类似：
 #\Application Data\\Application Data\\Application Data 找不到的错误
@@ -10,12 +9,13 @@ import os
 
 #path
 path = 'D:\\'
+
 #遍历目录树，先找单独大文件的，再找大文件夹的
+
 for folder, sub_folders, file_names in os.walk(path):
+    #重置
+    kong = 0
     for i in file_names:
-        try:
-            if os.path.getsize(os.path.join(folder, i)) > 10000000000:
-                print(i)
-        except:
-            print('not')
-            break
+        kong += os.path.getsize(os.path.join(folder, i))
+    if kong > 10000000000:
+        print(os.path.basename(folder) + str(kong))
