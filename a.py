0e744e68a72af599d6680536df9afe6e6a11d135
howolac <858103451@qq.com>
2021-03-09 00:23:54 +0800

    消除缺失的编号

--- a.py
+++ a.py
@@ -1,21 +1,47 @@
 #!python
 
-#项目：计算所有大于10gb的文件夹
-#usage：向path传入目录就行了
-#传入'c:\\users\\用户'会出现类似：
-#\Application Data\\Application Data\\Application Data 找不到的错误
+#项目：消除缺失的编号
+#描述：
+'''在一个文件夹中，找到所有带指定前缀的文件，诸如 spam001.txt,
+spam002.txt 等，并定位缺失的编号（例如存在 spam001.txt 和 spam003.txt，但不存
+在 spam002.txt）。让该程序对所有后面的文件改名，消除缺失的编号。
+'''
+#usage：输入指定文件夹
 
-import os
+import os, re, shutil
 
 #path
-path = 'D:\\'
+path = 'c:\\users\\howolac\\desktop'
 
-#遍历目录树，先找单独大文件的，再找大文件夹的
+#设一个筛选所有该类型文件的正则表达式
+#前缀设为'spam'
+regext = re.compile(r'^(spam)\d{3}(.txt)$')
+#筛选出所有该类型文件并加入列表
+kong = []
+for i in os.listdir(path):
+    if regext.search(i) != None:
+        kong += [i]
 
-for folder, sub_folders, file_names in os.walk(path):
-    #重置
-    kong = 0
-    for i in file_names:
-        kong += os.path.getsize(os.path.join(folder, i))
-    if kong > 10000000000:
-        print(os.path.basename(folder) + str(kong))
+# 对列表进行排序
+kong.sort()
+
+number = 1
+file_name = []
+#先获得所有（len(kong)）标准名字
+while (number - 1) < len(kong):
+    if number < 10:
+        file_name += ['spam' + '00' + str(number) + '.txt']
+    elif number < 100:
+        file_name += ['spam' + '0' + str(number) + '.txt']
+    else:
+        file_name += ['spam' + str(number) + '.txt']
+    number += 1
+
+#以列表长度遍历，如果不存在重命名
+for i in range(len(kong)):
+    if file_name != kong[i]:
+        for i in range(len(kong[i:])):
+            shutil.move(os.path.join(path, kong[i]),
+                        os.path.join(path, file_name[i]))
+    else:
+        continue
