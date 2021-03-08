9665044d7a282f64556af023c7ceaef0ccfc56e1
howolac <858103451@qq.com>
2021-03-08 13:27:47 +0800

    解压文件

--- a.py
+++ a.py
@@ -1,17 +1,14 @@
 #！python3
 #遍历目录树
-#usage：直接运行，浏览当前目录下所有且目录的目录所有且。。
+#usage：解压文件到桌面。注意先zip.namelist()之后才知道文件的名字，可能是a/a.txt
+#但不是a.txt
 
-import os
+import os, zipfile
 
-os.chdir('D:\\GAME')
+os.chdir('c:\\users\\howolac\\desktop')
 
-#读取当前目录字符串，子文件夹name列表，其它文件name列表
-for foderName, subfolders, fileNames in os.walk(os.getcwd()):
-    #打印出：说明：当前目录
-    print('current folder is : ' + foderName)
-    #遍历打印出：当前目录的文件路径+‘：’+文件
-    for subfolder in subfolders:
-        print(foderName + 'sufolders :' + subfolder)
-    for fileName in fileNames:
-        print(foderName + 'file :' + fileName)
+zip = zipfile.ZipFile('a.zip')
+
+
+zip.extract('a/a.txt')
+zip.close()
