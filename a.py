163e757b4206a63f78396fc9f4f165f82c27e7f6
howolac <858103451@qq.com>
2021-03-12 20:16:13 +0800

    打印表格简介版

--- a.py
+++ a.py
@@ -8,11 +8,9 @@ os.chdir('c:\\users\\howolac\\desktop')
 wb = openpyxl.load_workbook('1.xlsx')
 sheet1 = wb.get_sheet_by_name('Sheet1')
 
-#print(tuple(sheet1['A1':'C7']))
+#tuple(sheet['A1':'C3'])
 #遍历所有行
-for j in range(sheet1.max_row):
-    for i in range(sheet1.max_column):
-        print(get_column_letter(i + 1) + ' ' + str(i + 1), end='')
-        print(sheet1.cell(row=j + 1, column=i + 1).value)
-    print('---------------end of row---------------')
-#每一在结尾打印出结束信息
+for j in sheet1['A1':'C7']:
+    for i in j:
+        print(i.coordinate, i.value)
+    print('---------------------------end of row---------------------------')
